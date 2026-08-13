a1ef4ec0b06fd32785db62370f44badc9ca3e5e6
sergiiiavt <sergii.iavt@gmail.com>
2026-08-13 23:50:51 +0300

    examples

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello')
+x=input('msg: ')
+print('{0} Hello \n'.format(x))
+
+print("""This is a multi-line
+string example.
+Hello {0}""".format(x))
+
+age = input('Enter your age: ')
+print (int(age) + 5)
+
+print('555'.upper())
