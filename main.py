ec51144f0d4781b86147e96dc6a1cfe8faa748e5
sergiiiavt <sergii.iavt@gmail.com>
2026-08-17 10:41:48 +0300

    refactor main.py to improve code structure and readability

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+print(f"2 + 3 = {2 + 3}")
+
+arr = '1,2,3'.split(',')
+print(arr)
+
+print("#" * 20)
+
+msg = """a
+b
+c
+d
+d
+"""
+
+print(msg)
+print(type(msg))
+print(msg.count('d'))
+
+print(msg.replace('d', 'D'))
+
+from datetime import date
+date = date(2024, 6, 1)
+print(type(date))
+print(date.day)
+print(date.isoformat().split('-'))
+
 x=input('msg: ')
 print('{0} Hello \n'.format(x))
-
 print("""This is a multi-line
 string example.
 Hello {0}""".format(x))
@@ -9,3 +34,8 @@ age = input('Enter your age: ')
 print (int(age) + 5)
 
 print('555'.upper())
+
+print(type('555'.lower()))
+
+x = 333
+print('The value of x is: {0}'.format(x))
