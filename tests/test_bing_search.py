ea3521c336ac6f052ff7a6340893d0f3c7133e52
Sergii <77669556+sergiiiavt@users.noreply.github.com>
2026-08-18 23:29:05 +0300

    Use custom fixture in Bing search test

--- tests/test_bing_search.py
+++ tests/test_bing_search.py
@@ -1,15 +1,13 @@
 import re
 import allure
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import expect
 from pages.bing_search_page import BingSearchPage
 
 
 @allure.title("Search Playwright in Bing")
 @allure.feature("Search")
-def test_bing_search(page: Page):
-    bing_page = BingSearchPage(page)
-
+def test_bing_search(bing_page: BingSearchPage):
     bing_page.open()
     bing_page.search("playwright")
 
