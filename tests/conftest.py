634cd118bfb62518035e2860bcbfda652594d127
Sergii <77669556+sergiiiavt@users.noreply.github.com>
2026-09-20 14:26:15 +0300

    chore: move tests/conftest.py into organized structure

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from playwright.sync_api import Page
-
-from pages.bing_search_page import BingSearchPage
-
-
-@pytest.fixture
-def bing_page(page: Page) -> BingSearchPage:
-    return BingSearchPage(page)
